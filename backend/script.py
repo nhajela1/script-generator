b65e11b99e8957cb5a354d301a0cd3a31cfef424
Neil Hajela <70550790+nhajela1@users.noreply.github.com>
2024-07-27 14:58:08 -0400

    created backend

---
+++ backend/script.py
@@ -0,0 +1,20 @@
+from dotenv import load_dotenv
+import os
+from openai import OpenAI
+# Load environment variables from .env file
+load_dotenv()
+# Get the API key from environment variables
+api_key = os.getenv('OPENAI_API_KEY')
+client = OpenAI(
+    api_key=api_key
+)
+
+completion = client.chat.completions.create(
+  model="gpt-4o-mini",
+  messages=[
+    {"role": "system", "content": "You are a content creation tool that will try to produce high-performing scripts for short-form video platforms such as Instagram Reels, TikTok, and YouTube Shorts. The user will provide you with these parameters in the form of a Python string array: Genre, Title, Description, Length (in seconds). You will tailor your script to work well with the presumed audience demographics (based on the aforementioned parameters). Specifically, you will do your best to optimize for the appropriate tone and vocabulary depending on the parameters."},
+    {"role": "user", "content": "['Education', 'The History of the Internet', 'A brief overview of the history of the internet, from its inception to the present day.', '30']"}
+  ]
+)
+
+print(completion.choices[0].message)
