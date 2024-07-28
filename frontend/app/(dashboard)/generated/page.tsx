0c50a487122b3b6b6f55241c62c640bb0bcea36a
fortune710 <fortune0208@yahoo.com>
2024-07-27 20:36:47 -0400

    fetched user scripts

--- frontend/app/(dashboard)/generated/page.tsx
+++ frontend/app/(dashboard)/generated/page.tsx
@@ -1,10 +1,29 @@
+'use client'
+
+import { useQuery } from '@tanstack/react-query'
 import ScriptsTable from "@/components/scripts-table";
+import { getGeneratedScripts } from '@/app/api/scriptService'
+import { useAuth } from '@/hooks/auth';
 
 export default function GeneratedScriptsPage() {
+
+    const { getCurrentUser } = useAuth();
+
+    const { data: user } = useQuery({
+        queryKey: ['user'],
+        queryFn: async () => await getCurrentUser(),
+    })
+    
+    const { data: scripts, isLoading, error } = useQuery({
+        queryKey: ['scripts'],
+        queryFn: async () => await getGeneratedScripts(user?.id!),
+        enabled: !!user
+    })
+
     return (
         <main className="py-5">
             <section className="px-3 md:px-7">
-                <ScriptsTable/>
+                <ScriptsTable scripts={scripts} isLoading={isLoading} error={error} />
             </section>
         </main>
     )
